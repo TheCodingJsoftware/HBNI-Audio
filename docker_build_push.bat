a6d1a2d5462fd06a66f650415391b99e46c272a7
JareBear <jaredgrozz@gmail.com>
2025-01-03 09:07:54 -0600

    version control

--- docker_build_push.bat
+++ docker_build_push.bat
@@ -1,7 +1,8 @@
 @echo off
-VERSION=2.0.1
-docker build -t hbni-audio-archive:$VERSION .
-docker tag hbni-audio-archive:$VERSION jarebear/hbni-audio-archive:$VERSION
-docker tag hbni-audio-archive:$VERSION jarebear/hbni-audio-archive:latest
-docker push jarebear/hbni-audio-archive:$VERSION
+set VERSION=2.0.1
+
+docker build -t hbni-audio-archive:%VERSION% .
+docker tag hbni-audio-archive:%VERSION% jarebear/hbni-audio-archive:%VERSION%
+docker tag hbni-audio-archive:%VERSION% jarebear/hbni-audio-archive:latest
+docker push jarebear/hbni-audio-archive:%VERSION%
 docker push jarebear/hbni-audio-archive:latest
