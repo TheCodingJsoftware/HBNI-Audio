9900341a51b7b2751320d4fd87df761f4adafd30
JareBear <jaredgrozz@gmail.com>
2025-01-04 19:23:51 -0600

    latest tag

--- docker_build_push.bat
+++ docker_build_push.bat
@@ -6,7 +6,12 @@ for /f "tokens=2 delims==" %%I in ('"wmic os get localdatetime /value | findstr 
 :: Format the version as year.month.day.hour
 set VERSION=%datetime:~0,4%.%datetime:~4,2%.%datetime:~6,2%.%datetime:~8,2%
 
-:: Build, tag, and push the Docker image
+:: Build the Docker image with the versioned tag
 docker build -t hbni-audio-archive:%VERSION% .
-docker tag hbni-audio-archive:%VERSION% jarebear/hbni-audio-archive:%VERSION%
+
+:: Tag the Docker image as "latest"
+docker tag hbni-audio-archive:%VERSION% jarebear/hbni-audio-archive:latest
+
+:: Push both the versioned tag and the "latest" tag
 docker push jarebear/hbni-audio-archive:%VERSION%
+docker push jarebear/hbni-audio-archive:latest
